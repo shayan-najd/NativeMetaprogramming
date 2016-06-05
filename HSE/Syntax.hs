22ca504f77ce9cd8b03e06df86ec2ac8ab1747c3
Shayan-Najd <sh.najd@ed.ac.uk>
2016-06-05 19:45:41 +0100

    removing record syntax

--- HSE/Syntax.hs
+++ HSE/Syntax.hs
@@ -402,7 +402,8 @@ data InstDecl l
 
     | InsData   l (DataOrNew l) (Type l) [QualConDecl l] (Maybe (Deriving l))
 
-    | InsGData  l (DataOrNew l) (Type l) (Maybe (Kind l)) [GadtDecl l] (Maybe (Deriving l))
+    | InsGData  l (DataOrNew l) (Type l) (Maybe (Kind l)) [GadtDecl l]
+                  (Maybe (Deriving l))
 
 data BangType l
      = BangedTy   l
